d0f6a0329ac00b2a1be8b622ef36e5544003214a
cy <65646317+cyrsbly@users.noreply.github.com>
2021-08-27 20:25:01 +0800

    Update play.py

--- Group_Music_Probot/modules/play.py
+++ Group_Music_Probot/modules/play.py
@@ -544,7 +544,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/6fafdf6d8bd439b47f94c.jpg"
+        thumb_name = "https://telegra.ph/file/d9e6888b74e41cd439aa9.png"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
